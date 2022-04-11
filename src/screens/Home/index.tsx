14bd5fed93651e5d84e84ea5170cb3342682d34e
GabrielMatos10 <gabriel_de_pm@hotmail.com>
2022-04-11 12:44:01 -0300

    desafio completo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,91 +1,106 @@
-import React, { useState, useCallback } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useState, useCallback } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
-import { Header } from '../../components/Header';
-import { SearchBar } from '../../components/SearchBar';
-import { LoginDataItem } from '../../components/LoginDataItem';
+import { Header } from "../../components/Header";
+import { SearchBar } from "../../components/SearchBar";
+import { LoginDataItem } from "../../components/LoginDataItem";
 
 import {
-  Container,
-  Metadata,
-  Title,
-  TotalPassCount,
-  LoginList,
-} from './styles';
+	Container,
+	Metadata,
+	Title,
+	TotalPassCount,
+	LoginList,
+} from "./styles";
 
 interface LoginDataProps {
-  id: string;
-  service_name: string;
-  email: string;
-  password: string;
+	id: string;
+	service_name: string;
+	email: string;
+	password: string;
 }
 
 type LoginListDataProps = LoginDataProps[];
 
 export function Home() {
-  const [searchText, setSearchText] = useState('');
-  const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
-  const [data, setData] = useState<LoginListDataProps>([]);
+	const [searchText, setSearchText] = useState("");
+	const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
+	const [data, setData] = useState<LoginListDataProps>([]);
 
-  async function loadData() {
-    const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
-  }
+	async function loadData() {
+		// Get asyncStorage data, use setSearchListData and setData
+		const dataKey = "@savepass:logins";
+		const response = await AsyncStorage.getItem(dataKey);
+		
+		if(response) {
+			const passwordData = JSON.parse(response);
+			setData(passwordData);
+			setSearchListData(passwordData);
+		}
 
-  function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
-  }
+	}
 
-  function handleChangeInputText(text: string) {
-    // Update searchText value
-  }
+	function handleFilterLoginData() {
+		const filteredData = searchListData.filter((data) => {
+			if (data.service_name.includes(searchText)) {
+				return data;
+			}
+		});
+		setSearchListData(filteredData);
+	}
 
-  useFocusEffect(useCallback(() => {
-    loadData();
-  }, []));
+	function handleChangeInputText(text: string) {
+		setSearchText(text);
+	}
 
-  return (
-    <>
-      <Header
-        user={{
-          name: 'Rocketseat',
-          avatar_url: 'https://i.ibb.co/ZmFHZDM/rocketseat.jpg'
-        }}
-      />
-      <Container>
-        <SearchBar
-          placeholder="Qual senha você procura?"
-          onChangeText={handleChangeInputText}
-          value={searchText}
-          returnKeyType="search"
-          onSubmitEditing={handleFilterLoginData}
+	useFocusEffect(
+		useCallback(() => {
+			loadData();
+		}, [])
+	);
 
-          onSearchButtonPress={handleFilterLoginData}
-        />
+	return (
+		<>
+			<Header
+				user={{
+					name: "Rocketseat",
+					avatar_url: "https://i.ibb.co/ZmFHZDM/rocketseat.jpg",
+				}}
+			/>
+			<Container>
+				<SearchBar
+					placeholder="Qual senha você procura?"
+					onChangeText={handleChangeInputText}
+					value={searchText}
+					returnKeyType="search"
+					onSubmitEditing={handleFilterLoginData}
+					onSearchButtonPress={handleFilterLoginData}
+				/>
 
-        <Metadata>
-          <Title>Suas senhas</Title>
-          <TotalPassCount>
-            {searchListData.length
-              ? `${`${searchListData.length}`.padStart(2, '0')} ao total`
-              : 'Nada a ser exibido'
-            }
-          </TotalPassCount>
-        </Metadata>
+				<Metadata>
+					<Title>Suas senhas</Title>
+					<TotalPassCount>
+						{searchListData.length
+							? `${`${searchListData.length}`.padStart(2, "0")} ao total`
+							: "Nada a ser exibido"}
+					</TotalPassCount>
+				</Metadata>
 
-        <LoginList
-          keyExtractor={(item) => item.id}
-          data={searchListData}
-          renderItem={({ item: loginData }) => {
-            return <LoginDataItem
-              service_name={loginData.service_name}
-              email={loginData.email}
-              password={loginData.password}
-            />
-          }}
-        />
-      </Container>
-    </>
-  )
+				<LoginList
+					keyExtractor={(item) => item.id}
+					data={searchListData}
+					renderItem={({ item: loginData }) => {
+						return (
+							<LoginDataItem
+								service_name={loginData.service_name}
+								email={loginData.email}
+								password={loginData.password}
+							/>
+						);
+					}}
+				/>
+			</Container>
+		</>
+	);
 }
--- src/screens/RegisterLoginData/index.tsx
+++ src/screens/RegisterLoginData/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { Alert, KeyboardAvoidingView, Platform } from 'react-native';
 import { useForm } from 'react-hook-form';
 import { RFValue } from 'react-native-responsive-fontsize';
@@ -46,10 +46,28 @@ export function RegisterLoginData() {
       id: String(uuid.v4()),
       ...formData
     }
+    
+		try {
+      const dataKey = '@savepass:logins';        
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = JSON.parse(data) || [];
+    
+      const dataFormatted = [
+        ...currentData,
+        newLoginData
+      ]
+  
+      await AsyncStorage.setItem(
+        dataKey, 
+        JSON.stringify(dataFormatted)
+      )
 
-    const dataKey = '@savepass:logins';
+      navigate('Home')
 
-    // Save data on AsyncStorage and navigate to 'Home' screen
+		} catch (error) {
+			console.log(error);
+			Alert.alert("Não foi possível salvar");
+		}
   }
 
   return (
@@ -65,10 +83,7 @@ export function RegisterLoginData() {
             testID="service-name-input"
             title="Nome do serviço"
             name="service_name"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
+            error={errors.service_name && errors.service_name.message}
             control={control}
             autoCapitalize="sentences"
             autoCorrect
@@ -78,8 +93,7 @@ export function RegisterLoginData() {
             title="E-mail"
             name="email"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.email && errors.email.message
             }
             control={control}
             autoCorrect={false}
@@ -91,8 +105,7 @@ export function RegisterLoginData() {
             title="Senha"
             name="password"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.password && errors.password.message
             }
             control={control}
             secureTextEntry
